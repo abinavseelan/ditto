14daecf89aeeaa7bc7c5e25665281d3fb6641fb9
Abinav Seelan <abinav.n.seelan@gmail.com>
2020-02-02 12:46:14 +0530

    Migrate from rollup to webpack for server configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports =  {
   parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
   extends:  [
+    'eslint:recommended',
     'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
   ],
+  env: {
+    browser: true,
+    node: true,
+  },
   parserOptions:  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
     sourceType:  'module',  // Allows for the use of imports
